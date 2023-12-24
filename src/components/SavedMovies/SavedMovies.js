94c4e912b82a99d6a83df8e1f943478e187ce7e4
taniauvya <taniauvya@yandex.ru>
2023-12-24 23:55:57 +0300

    CSS -SavedMovies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,9 +1,25 @@
 import './SavedMovies.css'
-import Movies from '../Movies/Movies';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
 const SavedMovies = () => {
     return (
-        <Movies />
+        <div className="savedmovies">
+            <div className="savedmovies__container">
+                <Header />
+                <div className='savedmovies__container_searchform'>
+                    <SearchForm />
+                </div>
+                <div className='savedmovies__container_cardlist'>
+                    <MoviesCardList />
+                </div>
+                <div className='savedmovies__container_footer'>
+                    <Footer />
+                </div>
+            </div>
+        </div>
     );
 }
 
