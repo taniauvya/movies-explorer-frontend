d8e19ebcd447ce7dbefaa7f40942431280a4138a
taniauvya <taniauvya@yandex.ru>
2023-12-25 15:49:51 +0300

    CSS - Promo 768

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css'
 import imgLogo from '../../images/logo.svg'
 import Navigation from '../Navigation/Navigation';
 import NavigationMenu from '../NavigationMenu/NavigationMenu'
+import { CurrentUserContext } from '../../contexts/CurrentUserContext.js';
 
-const Header = ({ navigationLinkClass, navigationAccountClass, isBlackLineNavMenu =true }) => {
+const Header = ({ navigationLinkClass, navigationAccountClass, isBlackLineNavMenu = true }) => {
+
+    const currentUser = React.useContext(CurrentUserContext);
+
+    let linksElem;
+    if (currentUser?.name === '') {
+        linksElem =
+            <div className="header__container_login">
+                <div className="header__container_login_links">
+                    <Link to="/signup" className="header__register_link">Регистрация</Link>
+                    <Link to="/signin" className="header__login_link">Войти</Link>
+                </div>
+            </div>
+    }
+    else {
+        linksElem =
+            <>
+                <div className='header__container_nav_large'>
+                    <Navigation
+                        linkClass={navigationLinkClass}
+                        accountClass={navigationAccountClass} />
+                </div>
+                <div className='header__container_nav_medium'>
+                    <NavigationMenu isBlack={isBlackLineNavMenu} />
+                </div>
+            </>
+    }
 
     return (
         <div className="header">
             <img alt="logo" src={imgLogo} className='header__logo' />
 
-            <div className='header__container_nav_large'>
-                <Navigation
-                    linkClass={navigationLinkClass}
-                    accountClass={navigationAccountClass} />
-            </div>
-
-            <div className='header__container_nav_medium'>
-                <NavigationMenu isBlack={isBlackLineNavMenu} />
-            </div>
+            {linksElem}
         </div>
     )
 };
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,43 +1,25 @@
-import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import './Navigation.css'
 import imgAccountButton from '../../images/account-button.svg'
-import { CurrentUserContext } from '../../contexts/CurrentUserContext.js';
 
 const Navigation = ({ linkClass, accountClass }) => {
 
-    const currentUser = React.useContext(CurrentUserContext);
     const linkClassNav = ({ isActive }) => `${linkClass || ""} navigation__link ${isActive ? "navigation__link_active" : ""}`;
 
-    let linksElem;
-    if (currentUser?.name === '') {
-        linksElem =
-            <div className="navigation__container_login">
-                <Link to="/signup" className="navigation__register_link">Регистрация</Link>
-                <Link to="/signin" className="navigation__login_link">Войти</Link>
+    return (
+        <div className="navigation">
+            <div className='navigation__space' />
+            <div className="navigation___container_link">
+                <NavLink to="/movies" className={linkClassNav}>Фильмы</NavLink>
+                <NavLink to="/saved-movies" className={linkClassNav}>Сохранённые фильмы</NavLink>
             </div>
-    }
-    else {
-        linksElem =
+
             <div className={`navigation__account_container ${accountClass || ""}`}>
                 <Link to="/profile" className="navigation__account_link">Аккаунт</Link>
                 <div className="navigation__container_account_img">
                     <img alt="Аккаунт" className="navigation__account_img" src={imgAccountButton} />
                 </div>
             </div>
-    }
-
-    return (
-        <div className="navigation">
-            <div className='navigation__space' />
-            {(currentUser?.name !== '') &&
-                <div className="navigation___container_link">
-                    <NavLink to="/movies" className={linkClassNav}>Фильмы</NavLink>
-                    <NavLink to="/saved-movies" className={linkClassNav}>Сохранённые фильмы</NavLink>
-                </div>
-            }
-
-            {linksElem}
         </div>
     )
 };
