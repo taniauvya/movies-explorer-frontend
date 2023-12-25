81fc93f795b0fa96e52900194774b1dd00569b5a
taniauvya <taniauvya@yandex.ru>
2023-12-25 23:16:24 +0300

    CSS - LoginRegister 768

--- src/components/LoginRegisterForm/LoginRegisterForm.js
+++ src/components/LoginRegisterForm/LoginRegisterForm.js
@@ -5,7 +5,11 @@ import imgLogo from '../../images/logo.svg'
 const LoginRegisterForm = ({ title, formName, buttonText, onSubmit, linkTitle, linkText, linkAddress, children }) => {
     return (
         <div className="loginregister" tabIndex="0">
+
+            <div className='loginregister__space_top' />
+
             <div className="loginregister__container">
+
                 <div className="loginregister__container_logo">
                     <img className="loginregister__logo" src={imgLogo} />
                     <h1 className="loginregister__title">{title}</h1>
@@ -17,7 +21,9 @@ const LoginRegisterForm = ({ title, formName, buttonText, onSubmit, linkTitle, l
                         {children}
                     </div>
                     <p className="loginregister__error" />
-                    <div className='c123'/>
+                    
+                    <div className='loginregister__space'/>
+                    
                     <button aria-label={buttonText} className="loginregister__submit" type="submit">
                         {buttonText}
                     </button>
@@ -27,7 +33,10 @@ const LoginRegisterForm = ({ title, formName, buttonText, onSubmit, linkTitle, l
                     <p className='loginregister__link_title'>{linkTitle}</p>
                     <Link to={linkAddress} className='loginregister__link'>{linkText}</Link>
                 </div>
+
             </div>
+
+            <div className='loginregister__space_bottom'/>
         </div>
 
     );
