ae18a0a1c2fc6c5df526960c3ad72793f77e3b60
taniauvya <taniauvya@yandex.ru>
2023-12-27 17:48:58 +0300

    wip

--- src/components/LoginRegisterForm/LoginRegisterForm.js
+++ src/components/LoginRegisterForm/LoginRegisterForm.js
@@ -1,44 +1,35 @@
 import { Link } from 'react-router-dom';
 import './LoginRegisterForm.css';
-import imgLogo from '../../images/logo.svg'
 import Logo from '../Logo/Logo'
 
 
 const LoginRegisterForm = ({ title, formName, buttonText, onSubmit, linkTitle, linkText, linkAddress, children }) => {
     return (
         <div className="loginregister" tabIndex="0">
+            <header className='loginregister__header'>
+                <Logo />
+            </header>
 
-            <div className='loginregister__space_top' />
+            <main className="loginregister__main">
+                <h1 className="loginregister__title">{title}</h1>
 
-            <div className="loginregister__container">
-
-                <div className="loginregister__container_logo">
-                    <Logo />
-                    <h1 className="loginregister__title">{title}</h1>
-                </div>
-
-                <form name={formName} className="loginregister__form" onSubmit={onSubmit}>
-
+                <form name={formName} id={formName} className="loginregister__form" onSubmit={onSubmit}>
                     <div className="loginregister__container_input">
                         {children}
                     </div>
-                    <p className="loginregister__error" />
-
-                    <div className='loginregister__space' />
-
-                    <button aria-label={buttonText} className="loginregister__submit" type="submit">
-                        {buttonText}
-                    </button>
+                    <span className="loginregister__error" />
                 </form>
+            </main>
 
+            <footer className="loginregister__footer">
+                <button aria-label={buttonText} form={formName} className="loginregister__submit" type="submit">
+                    {buttonText}
+                </button>
                 <div className="loginregister___container_link">
                     <p className='loginregister__link_title'>{linkTitle}</p>
                     <Link to={linkAddress} className='loginregister__link'>{linkText}</Link>
                 </div>
-
-            </div>
-
-            <div className='loginregister__space_bottom' />
+            </footer>
         </div>
 
     );
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -13,44 +13,49 @@ const Profile = ({ handleNavigationMenuClick }) => {
     const currentUser = React.useContext(CurrentUserContext);
 
     const handleEnableSubmitClick = () => {
-        containerEditRef.current.classList.remove('profile__container_edit_visible');
-        containerSubmitRef.current.classList.add('profile__container_submit_visible');
+        containerEditRef.current.classList.remove('profile__edit_visible');
+        containerSubmitRef.current.classList.add('profile__submit_visible');
         inputNameRef.current.disabled = false;
         inputEmailRef.current.disabled = false;
     }
 
     return (
         <div className="profile">
-            <header className='header__container'>
+            <header className='profile__header'>
                 <Header handleNavigationMenuClick={handleNavigationMenuClick} />
             </header>
-            <main className="profile__container">
-                <h1 className="profile__title">{`Привет, ${currentUser.name}!`}</h1>
-                <form name="profile_edit" id="profile_edit" className="profile__form">
-                    <div className="profile__inputs_container">
-                        <div className="profile__input_container">
-                            <label className="profile__form_label">Имя</label>
-                            <input ref={inputNameRef} className="profile__form_input" type="text" name="email" defaultValue={currentUser.name} required disabled />
+
+            <main className="profile__main">
+                <div className="profile__container_top">
+                    <h1 className="profile__title">{`Привет, ${currentUser.name}!`}</h1>
+                    <form name="profile_edit" id="profile_edit" className="profile__form">
+                        <div className="profile__inputs_container">
+                            <div className="profile__input_container">
+                                <label className="profile__form_label">Имя</label>
+                                <input ref={inputNameRef} className="profile__form_input" type="text" name="email" defaultValue={currentUser.name} required disabled />
+                            </div>
+                            <div className="profile__input_separator" />
+                            <div className="profile__input_container">
+                                <label className="profile__form_label">E-mail</label>
+                                <input ref={inputEmailRef} className="profile__form_input" type="email" name="email" defaultValue={currentUser.email} required disabled />
+                            </div>
                         </div>
-                        <div className="profile__input_separator" />
-                        <div className="profile__input_container">
-                            <label className="profile__form_label">E-mail</label>
-                            <input ref={inputEmailRef} className="profile__form_input" type="email" name="email" defaultValue={currentUser.email} required disabled />
-                        </div>
-                    </div>
-                </form>
-                <div className='profile__space' />
-            </main>
-            <footer className='profile__container_buttons'>
-                <div ref={containerEditRef} className='profile__container_edit profile__container_edit_visible'>
-                    <button aria-label="Редактировать" type="button" className="profile__button_edit" onClick={handleEnableSubmitClick}>
-                        Редактировать
-                    </button>
-                    <button className='profile__button_logout' type="button">Выйти из аккаунта</button>
+                    </form>
                 </div>
-                <div ref={containerSubmitRef} className='profile__container_submit'>
-                    <span className='profile__error_submit' />
-                    <button className='profile__button_submit' for="profile_edit" type="submit">Сохранить</button>
+            </main>
+
+            <footer className='profile__footer'>
+                <div className="profile__container_top">
+                    <div ref={containerEditRef} className='profile__edit profile__edit_visible'>
+                        <button aria-label="Редактировать" type="button" className="profile__button_edit" onClick={handleEnableSubmitClick}>
+                            Редактировать
+                        </button>
+                        <button className='profile__button_logout' type="button">Выйти из аккаунта</button>
+                    </div>
+                    <div ref={containerSubmitRef} className='profile__submit'>
+                        <span className='profile__error_submit' />
+                        <button className='profile__button_submit' form="profile_edit" type="submit">Сохранить</button>
+                    </div>
                 </div>
             </footer>
         </div>
