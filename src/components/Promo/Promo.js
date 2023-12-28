db58c91ebb3dc5c4940d6d9b3a4fa865382b066d
taniauvya <taniauvya@yandex.ru>
2023-12-28 16:53:49 +0300

    wip

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,30 +6,36 @@ import imgWorldLogo from '../../images/logo-world.svg'
 const Promo = ({ handleNavigationMenuClick }) => {
     return (
         <div className="promo">
-            <div className="promo__container">
+            <header className='promo__header'>
                 <Header
                     navigationAccountClass="navigation_account_color"
                     navigationLinkClass="navigation_link_color"
                     isBlackLineNavMenu={false}
                     handleNavigationMenuClick={handleNavigationMenuClick} />
-                <div className="promo__container_content">
-                    <div className="promo__container_title">
-                        <h1 className="promo__title_main">
-                            Учебный проект студента
-                            факультета
-                            Веб&#8209;разработки.
-                        </h1>
-                        <h2 className="promo__text">
-                            Листайте ниже, чтобы узнать больше про этот
-                            проект и его создателя.
-                        </h2>
+            </header>
+            <main className='promo__main'>
+                <div className="promo__container_main">
+                    <div className="promo__space_main_top" />
+                    <div className="promo__container_content">
+                        <div className="promo__container_title">
+                            <div className="promo__space_main_title" />
+                            <h1 className="promo__title_main">
+                                Учебный проект студента
+                                факультета
+                                Веб&#8209;разработки.
+                            </h1>
+                            <div className="promo__space_main_text" />
+                            <h2 className="promo__text">
+                                Листайте ниже, чтобы узнать больше про этот
+                                проект и его создателя.
+                            </h2>
+                        </div>
+                        <img alt="Логотип" src={imgWorldLogo} className="promo__logo" />
                     </div>
-                    <img alt="Логотип" src={imgWorldLogo} className="promo__logo" />
-                </div>
-                <div className="promo__container_more">
+                    <div className="promo__space_more" />
                     <Link to="#aboutproject" className="promo__link_more">Узнать больше</Link>
                 </div>
-            </div>
+            </main>
         </div>
     )
 };
