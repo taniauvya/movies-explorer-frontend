6c501480ce4f9ad375a30dd8113340db2bbb44e6
taniauvya <taniauvya@yandex.ru>
2023-12-29 04:23:30 +0300

    AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -7,34 +7,41 @@ import imgAvatar from '../../images/student-photo.png'
 const AboutMe = () => {
     return (
         <div className="aboutme">
-            <div className="aboutme__container">
+
+            <header className='aboutme__header'>
+                <div className='aboutme__space aboutme__space_header_top' />
                 <MainTitle title="Студент" />
-                <div className="aboutme__about">
-                    <div className="aboutme__about_text">
-                        <h1 className="aboutme__h1">
-                            Виталий
-                        </h1>
-                        <h2 className="aboutme__h2">
-                            Фронтенд-разработчик, 30 лет
-                        </h2>
-                        <p className="aboutme__p">
-                            Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-                            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С
-                            2015 года работал в компании «СКБ Контур». После того, как прошёл курс по
-                            веб&#8209;разработке, начал заниматься фриланс&#8209;заказами и ушёл с постоянной работы.
-                        </p>
-                        <Link className="aboutme__link" to="https://github.com/taniauvya" target="_blank">Github</Link>
+            </header>
+
+            <main className="aboutme__main">
+                <div className="aboutme__container_main">
+                    <div className="aboutme__about">
+                        <div className="aboutme__about_text">
+                            <h1 className="aboutme__h1">
+                                Виталий
+                            </h1>
+                            <h2 className="aboutme__h2">
+                                Фронтенд-разработчик, 30 лет
+                            </h2>
+                            <p className="aboutme__p">
+                                Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
+                                и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С
+                                2015 года работал в компании «СКБ Контур». После того, как прошёл курс по
+                                веб&#8209;разработке, начал заниматься фриланс&#8209;заказами и ушёл с постоянной работы.
+                            </p>
+                            <Link className="aboutme__link" to="https://github.com/taniauvya" target="_blank">Github</Link>
+                        </div>
+                        <img alt="Аватар" src={imgAvatar} className='aboutme__about_avatar' />
                     </div>
-                    <img alt="Аватар" src={imgAvatar} className='aboutme__about_avatar' />
+                    <div className="aboutme__portfolio">
+                        <p className="aboutme__portfolio_title">Портфолио</p>
+                        <Portfolio title="Статичный сайт" link="https://github.com/" />
+                        <Portfolio title="Адаптивный сайт" link="https://github.com/" />
+                        <Portfolio title="Одностраничное приложение" link="https://github.com/" />
+                    </div>
                 </div>
-                <div className="aboutme__portfolio">
-                    <p className="aboutme__portfolio_title">Портфолио</p>
-                    <Portfolio title="Статичный сайт" link="https://github.com/" />
-                    <Portfolio title="Адаптивный сайт" link="https://github.com/" />
-                    <Portfolio title="Одностраничное приложение" link="https://github.com/" />
-                </div>
-            </div>
-        </div >
+            </main>
+        </div>
     )
 };
 
