bd38cc32f3b7c7a066163ac58ea7ea7b29eeb00b
taniauvya <taniauvya@yandex.ru>
2023-12-31 03:49:47 +0300

    AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -15,30 +15,40 @@ const AboutMe = () => {
 
             <main className="aboutme__main">
                 <div className="aboutme__container_main">
+                    <div className='aboutme__space aboutme__space_main_top' />
                     <div className="aboutme__about">
                         <div className="aboutme__about_text">
+                        <div className='aboutme__space aboutme__space_h1' />
                             <h1 className="aboutme__h1">
                                 Виталий
                             </h1>
+                            <div className='aboutme__space aboutme__space_h2' />
                             <h2 className="aboutme__h2">
                                 Фронтенд-разработчик, 30 лет
                             </h2>
+                            <div className='aboutme__space aboutme__space_p' />
                             <p className="aboutme__p">
                                 Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
                                 и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С
                                 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по
                                 веб&#8209;разработке, начал заниматься фриланс&#8209;заказами и ушёл с постоянной работы.
                             </p>
+                            <div className='aboutme__space aboutme__space_link' />
                             <Link className="aboutme__link" to="https://github.com/taniauvya" target="_blank">Github</Link>
                         </div>
                         <img alt="Аватар" src={imgAvatar} className='aboutme__about_avatar' />
                     </div>
+                    
+                    <div className='aboutme__space aboutme__space_portfolio_title' />
+                    <p className="aboutme__portfolio_title">Портфолио</p>
+                    
+                    <div className='aboutme__space aboutme__space_portfolio' />
                     <div className="aboutme__portfolio">
-                        <p className="aboutme__portfolio_title">Портфолио</p>
                         <Portfolio title="Статичный сайт" link="https://github.com/" />
                         <Portfolio title="Адаптивный сайт" link="https://github.com/" />
-                        <Portfolio title="Одностраничное приложение" link="https://github.com/" />
+                        <Portfolio title="Одностраничное приложение" link="https://github.com/" hasUnderline={false} />
                     </div>
+                    <div className='aboutme__space aboutme__space_main_bottom' />
                 </div>
             </main>
         </div>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom'
 import './Portfolio.css'
 
-const Portfolio = (props) => {
+const Portfolio = ({ link, title, hasUnderline = true }) => {
     return (
         <div className="portfolio">
             <div className="portfolio__line">
-                <Link className="portfolio__title" to={props.link} target="_blank">
-                    {props.title}
+                <Link className="portfolio__title" to={link} target="_blank">
+                    {title}
                 </Link>
-                <Link className="portfolio__link" to={props.link} target="_blank"></Link>
+                <Link className="portfolio__link" to={link} target="_blank"></Link>
             </div>
-            <div className="portfolio__underline" />
+            {hasUnderline && <div className="portfolio__underline" />}
         </div>
     )
 };
